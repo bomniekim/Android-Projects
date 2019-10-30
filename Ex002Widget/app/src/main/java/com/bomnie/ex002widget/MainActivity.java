8a0a065811f0f565cac1213e8d0b3f1c75a28b42
BominKim <bomnie.kim@gmail.com>
2019-10-30 17:40:23 +0900

    add Ex002widget

---
+++ Ex002Widget/app/src/main/java/com/bomnie/ex002widget/MainActivity.java
@@ -0,0 +1,20 @@
+package com.bomnie.ex002widget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        //XML에서 만든 TextView를 참조하여 제어하기
+        TextView tv= findViewById(R.id.tv);
+        tv.setSelected(true);
+    }
+}
